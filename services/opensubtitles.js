b21bca0d31964c3d6a0a9f7e0043c0e3254a5524
Francis-play <93499490+Francis-play@users.noreply.github.com>
2025-05-22 18:11:20 -0400

    Update opensubtitles.js

--- services/opensubtitles.js
+++ services/opensubtitles.js
@@ -1,88 +1,58 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const getSuggestions = async (query) => {
-  const url = `https://www.opensubtitles.org/libs/suggest.php?format=json3&MovieName=${encodeURIComponent(query)}&SubLanguageID=spn,spl`;
-  const res = await axios.get(url);
-  return res.data;
-};
+const BASE_URL = 'https://www.opensubtitles.org';
 
-const getSubtitlesByImdb = async (imdbid) => {
-  const searchUrl = `https://www.opensubtitles.org/en/search/sublanguageid-spn,spl/imdbid-${imdbid}`;
-  const { data: html } = await axios.get(searchUrl);
-  const $ = cheerio.load(html);
+async function searchSubtitles(imdbid) {
+  const lang = 'spn,spl';
+  const url = `${BASE_URL}/en/search/sublanguageid-${lang}/imdbid-${imdbid}`;
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
 
-  const results = [];
+  const subtitles = [];
 
-  $('a.bnone').each((i, el) => {
-    const href = $(el).attr('href');
-    if (!href.includes('/en/subtitles/')) return;
+  $('a.bnone').each((_, el) => {
+    const subtitleUrl = $(el).attr('href');
+    const idMatch = subtitleUrl.match(/\/subtitles\/(\d+)\//);
+    const subtitleId = idMatch ? idMatch[1] : null;
+    const filename = $(el).text().trim();
+    const lang = $(el).closest('tr').find('td:nth-child(6)').text().trim();
 
-    const subtitleId = href.split('/')[3];
-    const subtitleUrl = `https://www.opensubtitles.org${href}`;
-    const downloadIdMatch = $(el).closest('tr').find('a[href*="/download/file/"]').attr('href');
-    const downloadUrl = downloadIdMatch
-      ? `https://dl.opensubtitles.org${downloadIdMatch}`
-      : null;
-
-    results.push({
-      subtitleId,
-      subtitleUrl,
-      downloadUrl,
-    });
+    if (subtitleId) {
+      subtitles.push({
+        subtitleId,
+        subtitleUrl: `${BASE_URL}${subtitleUrl}`,
+        language: lang,
+        filename,
+        downloadUrl: `${BASE_URL}/en/download/sub/${subtitleId}`,
+      });
+    }
   });
 
-  return results;
-};
+  return subtitles;
+}
 
-module.exports = {
-  getSuggestions,
-  getSubtitlesByImdb,
-};
+async function getSubtitleDetails(id) {
+  const url = `${BASE_URL}/en/subtitles/${id}`;
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
 
-const getSubtitleDetails = async (subtitleId) => {
-  const url = `https://www.opensubtitles.org/en/subtitles/${subtitleId}`;
-  const { data: html } = await axios.get(url);
-  const $ = cheerio.load(html);
+  const downloadMatch = response.data.match(/href="(\/en\/download\/file\/\d+)"/);
+  const downloadUrl = downloadMatch ? `${BASE_URL}${downloadMatch[1]}` : null;
 
-  const languageLink = $('a[title*="All subtitles for this movie in this language"]').first();
-  const language = languageLink.attr('title')?.match(/- (.*?) \(/)?.[1] || null;
-
-  const filename = $('#commenttext_' + subtitleId).text().trim() || null;
-
-  let release = null;
-  if (filename) {
-    const lowered = filename.toLowerCase();
-    if (lowered.includes('webrip')) release = 'WEBRip';
-    else if (lowered.includes('bluray')) release = 'BluRay';
-    else if (lowered.includes('hdrip')) release = 'HDRip';
-    else if (lowered.includes('dvdrip')) release = 'DVDRip';
-  }
-
-  const downloadMatch = html.match(/\/en\/download\/file\/(\d+)/);
-  const downloadUrl = downloadMatch ? `https://dl.opensubtitles.org/en/download/file/${downloadMatch[1]}` : null;
-
-  let nfo = null;
-  const nfoMatch = html.match(/\/en\/nfo\/(\d+)/);
-  if (nfoMatch) {
-    const nfoId = nfoMatch[1];
-    const nfoUrl = `https://www.opensubtitles.org/en/nfo/${nfoId}`;
-    try {
-      const { data: nfoHtml } = await axios.get(nfoUrl);
-      const _$ = cheerio.load(nfoHtml);
-      nfo = _$('.nfo pre').text().trim() || null;
-    } catch {
-      // nfo not found or error
-    }
-  }
+  const filename = $('title').text().split('subtitles - ')[1]?.split(' -')[0]?.trim() || 'Unknown';
+  const language = $('a[href*="sublanguageid"]').first().text().trim() || 'Unknown';
 
   return {
-    subtitleId,
+    subtitleId: id,
     subtitleUrl: url,
     downloadUrl,
-    language,
-    ...(filename && { filename }),
-    ...(release && { release }),
-    ...(nfo && { nfo })
+    filename,
+    language
   };
+}
+
+module.exports = {
+  searchSubtitles,
+  getSubtitleDetails
 };
